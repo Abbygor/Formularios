01a96d6ab565a55b69a65f629177b32a5db63f7b
Luis Antonio Feregrino <djlaf2008@hotmail.com>
2019-10-15 23:10:37 -0500

    TemplateComponent and DataComponent added, a form added to TemplateComponent

---
+++ src/app/components/template/template.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: []
+})
+export class TemplateComponent implements OnInit {
+
+  usuario: any = {
+    nombre: "Jose Luis",
+    apellido: "Feregrino",
+    email: ""
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  guardar(forma: NgForm) {
+    console.log("Formulario posteado");
+    console.log("ngForm",forma);
+    console.log("valor",forma.value);
+    console.log("usuario",this.usuario);
+  }
+
+}
