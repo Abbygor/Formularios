a2dd99d946f468df13de2d67fccf2a5ffa6fcb41
Luis Antonio Feregrino <jose.feregrino@encontrack.com>
2019-10-18 15:40:03 -0500

    Custom Synchronous validators added to Apellido and Repetir password field

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -24,15 +24,21 @@ export class DataComponent {
     this.forma = new FormGroup({
       nombreCompleto: new FormGroup({
         nombre: new FormControl(null, [Validators.required, Validators.minLength(3)]),
-        apellido: new FormControl(null, Validators.required),
+        apellido: new FormControl(null, [Validators.required, this.noFeregrino]),
       }),
       correo: new FormControl(null, [Validators.required,
                                   Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
       pasatiempos: new FormArray([
         new FormControl('Correr', Validators.required)
-      ])
+      ]),
+      password1: new FormControl(null, Validators.required),
+      password2: new FormControl()
     });
 
+    this.forma.controls['password2'].setValidators([
+      Validators.required,
+      this.noIgual.bind(this.forma)
+    ]);
     //this.forma.setValue(this.usuario);
   }
 
@@ -42,11 +48,30 @@ export class DataComponent {
     );
   }
 
+  noFeregrino(control: FormControl): {[s: string]: boolean} {
+    if(control.value === 'feregrino') {
+      return {
+        noferegrino: true
+      }
+    }
+    return null;
+  }
+
+  noIgual(control: FormControl): {[s: string]: boolean} {
+    let forma: any = this;
+    if(control.value !== forma.controls['password1'].value) {
+      return {
+        noiguales: true
+      }
+    }
+    return null;
+  }
+
   guardarCambios() {
     console.log(this.forma.value);
     console.log(this.forma.valid);
 
-    this.forma.reset(this.usuario);
+    //this.forma.reset(this.usuario);
   }
 
 }
